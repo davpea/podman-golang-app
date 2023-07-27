0fb7f3be288d79ccd271d0bc8b5dc58cef659467
David Pearson <davpears15@yahoo.com>
2023-07-27 16:04:00 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # podman-golang-app
-Build and run containerised go app based off the Red Hat RHEL 9 UBI using Podman Desktop 
+Build and run containerised go app based off the Red Hat RHEL 9 UBI using Podman Desktop
+
+I'm using code form https://github.com/alexellis and https://github.com/ksingh7  
---
+++ ubigoapp.go
@@ -0,0 +1,39 @@
+// Simple app that uses Red Hat UBI 9 image with golang to test Podman
+// This code origionally built by https://github.com/ksingh7
+
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	response := os.Getenv("RESPONSE")
+	if len(response) == 0 {
+		response = "Hi David - Your app is working!"
+	}
+
+	fmt.Fprintln(w, response)
+	fmt.Println("Servicing request.")
+}
+
+func listenAndServe(port string) {
+	fmt.Printf("serving on %s\n", port)
+	err := http.ListenAndServe(":"+port, nil)
+	if err != nil {
+		panic("ListenAndServe: " + err.Error())
+	}
+}
+
+func main() {
+	http.HandleFunc("/", helloHandler)
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
+		port = "8080"
+	}
+	go listenAndServe(port)
+
+	select {}
+}
